319d42fa443fc3a86533b03414fa2090be9609dc
asif7480 <a.a.khan7480@gmail.com>
2025-04-04 19:23:56 +0500

    Feat: Added Cloud Connection

--- backend/config/db.mjs
+++ backend/config/db.mjs
@@ -3,7 +3,8 @@ import { DB_NAME } from "../constants/constant.mjs";
 
 export const connectDB = async() => {
     try{
-        const connnectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        // const connnectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        const connnectionInstance = await mongoose.connect(`${process.env.MONGODB_CLOUD_URI}/${DB_NAME}`)
         console.log(`Connected to ${connnectionInstance.connection.host}`)
     }catch(err){
         console.log(`Error: ${err.message}`);
